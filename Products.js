beabcaa16f59d624b91be9b85bcc1fb88a287b9e
Zim67 <127262688+Zim67@users.noreply.github.com>
2023-06-06 00:06:01 -0230

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,37 @@
+const products = [
+    {
+        id: "p1",
+        name: "Canon EOS R5",
+        description: "Brand New Canon, has the quality",
+        price: 399,
+        discontinued: false,
+        categories: ["c1"],
+    },
+    {
+        id: "p2",
+        name: "MacBook Pro",
+        description: "Brand New Mackbook, Has the quality",
+        price: 5300,
+        discontinued: false,
+        categories: ["c1", "c2"],
+    },
+    {
+        id: "p3",
+        name: "Google Nest Hub",
+        description: "IBrand New Nest Hub, Has the quality",
+        price: 75,
+        discontinued: false,
+        categories: ["c1", "c3"],
+    },
+    {
+        id: "p4",
+        name: "Amazon Echo Show 10",
+        description: "Brand Amazon Echo show, Has the quality",
+        price: 100,
+        discontinued: false,
+        categories: ["c1", "c4"],
+    },
+  // Add more products here
+];
+
+export default products;
---
+++ categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  { id: "c1", description: "Category 1" },
+  { id: "c2", description: "Category 2" },
+  { id: "c3", description: "Category 3" },
+  { id: "c4", description: "Category 4" },
+];
+
+export default categories;
